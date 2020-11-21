87ed5d026e9b76628c0132fa03be0c8968741fb4
jaesonwatts <jaeson.watts@gmail.com>
2020-11-21 02:21:26 -0700

    end of night. still working on axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Registration from "./auth/Registration";
 
 export default class Home extends Component {
   render() {
@@ -6,6 +7,7 @@ export default class Home extends Component {
       <div>
         <h1>Home</h1>
         <h1>This is the home component yo!</h1>
+        <Registration />
       </div>
     );
   }
---
+++ src/components/auth/Registration.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { response } from 'express';
+
+export default class Registration extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      password_confirmation: ""
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    const {
+      email,
+      password,
+      password_confirmation
+    } = this.state
+    event.preventDefault();
+    axios
+      .post(
+        "https://127.0.0.1:3001/registrations",
+        {
+          user: {
+          email: email,
+          password: password,
+          password_confirmation: password_confirmation
+          }
+        },
+        { withCredentials: true }
+      )
+    .then(response => {
+      console.log("registration response", response);
+    })
+    .catch(error => {
+      console.log("registration error", error);
+    });
+  }
+
+  handleChange(event) {
+    
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="email" 
+            name="email" 
+            placeholder="Email" 
+            value={this.state.email} 
+            onChange={this.handleChange} 
+            required 
+          />
+
+              <input 
+            type="password" 
+            name="password" 
+            placeholder="Password" 
+            value={this.state.password} 
+            onChange={this.handleChange} 
+            required 
+          />
+
+<input 
+            type="password" 
+            name="password_confirmation" 
+            placeholder="Password Confirmation" 
+            value={this.state.password_confirmation} 
+            onChange={this.handleChange} 
+            required 
+          />
+          <button type='submit'>Register</button>
+
+        </form>
+      </div>
+      );
+  }
+}
